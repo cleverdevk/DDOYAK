34f37c0bcb615dd6c4c1758a51b50fc634322795
rnjsdnfka <39620363+rnjsdnfka@users.noreply.github.com>
2018-08-10 16:52:17 +0900

    edittext

---
+++ Realtime_Data/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.caucse.cccc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       editText = (EditText) findViewById(R.id.editText);
+
+
+        // Read from the database
+
+        myRef.setValue("하나");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("TAG", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException());
+            }
+        });
+    }
+    public void onClick(View view)
+    {
+        myRef.setValue(editText.getText().toString());
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ Realtime_Data/MedicineDetails.java
@@ -0,0 +1,19 @@
+package com.example.caucse.cccc;
+
+public class MedicineDetails {
+
+    private String Medicine_name;
+    private String Medicine_Frequency;
+
+    public MedicineDetails() {
+    }
+    public String getMedicine_name() {
+        return Medicine_name
+    }
+    public void setMedicine_name(String name) {
+        this.Medicine_name = name;
+    }
+    public String getMedicine_Frequency() {
+        return Me
+    }
+}
