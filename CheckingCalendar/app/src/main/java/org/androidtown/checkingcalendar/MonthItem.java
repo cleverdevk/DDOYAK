68cd7f845388d7bb27e141712da6fee1ce1c67db
cloudhs7 <heesu723@naver.com>
2018-09-02 12:53:19 +0900

    upload checking calendar(text OOX)

---
+++ CheckingCalendar/app/src/main/java/org/androidtown/checkingcalendar/MonthItem.java
@@ -0,0 +1,30 @@
+package org.androidtown.checkingcalendar;
+
+public class MonthItem {
+    private int dayValue;
+    private int overValue;
+
+    public MonthItem(){ }
+
+    public MonthItem(int dayValue, int overValue) {
+        this.dayValue = dayValue;
+        this.overValue= overValue;
+    }
+
+    public int getDay(){
+        return dayValue;
+    }
+
+    public int getOverValue(){
+        return overValue;
+    }
+
+    public void setDay(int dayValue){
+        this.dayValue = dayValue;
+    }
+
+    public void setOverValue(int overValue){
+        this.overValue = overValue;
+    }
+
+}
